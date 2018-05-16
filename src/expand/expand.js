3f95d25f16dd2ff8b57547b046ea29ece938cfa4
tdeekens <tobias.deekens@commercetools.de>
2018-05-16 15:08:20 +0200

    feat: add expand

--- src/expand/expand.js
+++ src/expand/expand.js
@@ -1,3 +1,13 @@
 import { shallow } from 'enzyme';
 
-export default () => {};
+export default ({ propName = 'children', props = {} } = {}) => {
+  const render = wrapper.prop(propName);
+
+  if (typeof render !== 'function')
+    throw new Error(
+      `@commercetools/enzyme-extensions/expand: the specified 'propName' "${propName}" is not a function on the wrapper.`
+    );
+  const nextWrapper = shallow(render(props));
+
+  return nextWrapper;
+};
