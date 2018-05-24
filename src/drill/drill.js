8922e7f2d28dbc06d449094d444efad852365464
Dominik Ferber <dominik.ferber@gmail.com>
2018-05-24 23:17:13 +0200

    fix(drill): rename renderProp leftovers to drill

--- src/drill/drill.js
+++ src/drill/drill.js
@@ -7,9 +7,9 @@ const shallowRenderProp = (wrapper, expander) =>
     ? wrapper.prop(expander)()
     : expander(wrapper.props());
 
-module.exports = function renderProp(expander) {
+module.exports = function drill(expander) {
   // throws an error if the current instance has a length other than one.
-  return this.single('renderProp', () =>
+  return this.single('drill', () =>
     // create a new wrapper with the same root as the current wrapper,
     // with any nodes passed in as the first parameter automatically wrapped
     this.wrap(shallowRenderProp(this, expander))
